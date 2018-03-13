310a4eb9462b4a544de611aad5d00cff884753c1
Jonas Andersen <hvidovrefifa@gmail.com>
2018-03-13 10:53:22 +0100

    Facade

---
+++ CA2Rest/src/main/java/facader/IcompaniesFacade.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facader;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Jonas
+ */
+public interface IcompaniesFacade {
+    public void addEntityManagerFactory(EntityManagerFactory emf);
+   public Companies addCompanie(Companies c);
+   public Companies editCompanie(Companies c);
+   public Companies deleteCompanie(int id);
+   public Companies getCompanie(int id);
+    public List<Companies> getEmployes();
+    public Companies getInfo(String phone);
+    
+    
+}
