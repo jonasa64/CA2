4a93b91d228bc157adabe2549b0bd00eb951b200
Jonas Andersen <hvidovrefifa@gmail.com>
2018-03-15 16:21:52 +0100

    javaScript for companie

--- CA2/src/main/webapp/javascript.js
+++ CA2/src/main/webapp/javascript.js
@@ -28,6 +28,64 @@ function GetPersonLivingIn(){
  }
  
  
+ // edit person 
+ function editPerson(id){
+     fetch("api/person" + id , { method: "put"}).then(function (response){
+         response.json();
+     }).then(function(data){
+         document.getElementById("edit").innerHTML = data.id;
+     })
+ }
+ 
+ // delete person
+ 
+ function deletePerson(id){
+     fetch("api/person"+id,{method:"delete"}).then(function (response){
+         return response.json();
+     }).then(function(data){
+         document.getElementById("delete").innerHTML = data.id;
+     })
+ }
+ 
+ // get person by id 
+
+ 
+ // get person by phone 
+ 
+ // get all persons 
+ document.getElementById("btn0").addEventListener("click",function(){
+     getAllPerson();
+ });
+ 
+ function getAllPerson(){
+     fetch("api/person",{method:"get"}).then(function(json){
+        document.getElementById("show").innerHTML = "";
+        
+        var rows = "";
+        
+        
+        for(var i = 0; i < json.persons; i++){
+            rows +='<tr>';
+                    
+                   rows += '<td>' + persons[i].firstName + '</td>';
+                   rows += '<td>' + persons[i].lastName + '</td>';
+                   rows +=  '<td>' + persons[i].phone + '</td>';
+                   rows += '<td>' + persons[i].email + '</td>';
+                    
+                    '<tr>' ;
+        }
+        
+        document.getElementById("show").innerHTML = rows;
+        
+     })
+ }
+ 
+ 
+ // get all persons by zipcode
+ 
+ 
+ 
+ 
  
  function showFrom(){
      document.getElementById("form").innerHTML = 
